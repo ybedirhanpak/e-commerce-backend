5b66f8da9864132bcf6024bf94e63f3c170a12c4
Alicem <krlbl_alicem@hotmail.com>
2019-07-16 11:44:38 +0300

    implements smpt gateway

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Mail;
+using System.Net;
+
 
 namespace e_commerce_api
 {
@@ -14,6 +18,24 @@ namespace e_commerce_api
     {
         public static void Main(string[] args)
         {
+            SmtpClient sc = new SmtpClient();
+            sc.Port = 587;
+            sc.Host = "smtp.gmail.com";
+            sc.EnableSsl = true;
+
+            sc.Credentials = new NetworkCredential("teknomarkt2019@gmail.com", "teknomarkt.2019!");
+
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("teknomarkt@gmail.com");
+
+            mail.To.Add("teknomarkt2019@gmail.com");
+
+            mail.Subject = "E-Posta Konusu"; mail.IsBodyHtml = true; mail.Body="E-Posta İçeriği" ;
+
+            sc.Send(mail);
+
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
