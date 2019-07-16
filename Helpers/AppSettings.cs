03f0a1789397a234ebc01d9f794f7055da770bd6
Furkan Gök <muhammet.gok@boun.edu.tr>
2019-07-16 15:05:19 +0300

    adds smtp gateway to send emails

--- Helpers/AppSettings.cs
+++ Helpers/AppSettings.cs
@@ -5,9 +5,18 @@ using System.Threading.Tasks;
 
 namespace e_commerce_api.Helpers
 {
-    public class AppSettings
+    public class AppSettings : IAppSettings
     {
         public string Secret { get; set; }
+        public string Email { get; set; }
+        public string EmailPassword { get; set; }
+
+    }
+    public interface IAppSettings
+    {
+        string Secret { get; set; }
+        string Email { get; set; }
+        string EmailPassword { get; set; }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -18,23 +18,7 @@ namespace e_commerce_api
     {
         public static void Main(string[] args)
         {
-            SmtpClient sc = new SmtpClient();
-            sc.Port = 587;
-            sc.Host = "smtp.gmail.com";
-            sc.EnableSsl = true;
-
-            sc.Credentials = new NetworkCredential("teknomarkt2019@gmail.com", "teknomarkt.2019!");
-
-            MailMessage mail = new MailMessage();
-
-            mail.From = new MailAddress("teknomarkt@gmail.com");
-
-            mail.To.Add("teknomarkt2019@gmail.com");
-
-            mail.Subject = "E-Posta Konusu"; mail.IsBodyHtml = true; mail.Body="E-Posta İçeriği" ;
-
-            sc.Send(mail);
-
+            
 
             CreateWebHostBuilder(args).Build().Run();
         }
