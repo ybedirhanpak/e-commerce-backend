f6644ad56f913c8c3d8c7c6a472b4c27f00d91d3
Furkan Gök <muhammet.gok@boun.edu.tr>
2019-07-18 11:41:37 +0300

    changes product fields and adds a new model named review

---
+++ Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace e_commerce_api.Models
+{
+    public class Review
+    {
+
+
+        public string userFullName { get; set; }
+
+        public string userMail { get; set; }
+
+        public string reviewContent { get; set; }
+
+        public string commentTime { get; set; }
+
+        public int numberOfStars { get; set; }
+    }
+}
